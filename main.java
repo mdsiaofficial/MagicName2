00089a4ff823c8c6fbe9fe86727fbdc2d40a5062
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-04-15 22:16:31 +0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,209 @@
+//Inspired from :https://code.sololearn.com/cozgL69YFEWm/?ref=app
+//Inspired from :https://code.sololearn.com/cMjHm80zOip1/?ref=app.
+//Actually these two above codes ate one of the most popular codes jn sololearn.I thought that this types of codes were made in C++ & Python
+//Input: Please type your name and just see magic....
+//Hope you will love it
+//FOLLOW ME PLEASE
+//This code is in JAVA Version
+//Dont input numbers⚠️⚠️☠️
+import java.io.*;
+public class namestyler
+{
+
+public static void main(String args [])throws IOException
+{
+
+String S;
+int N;
+char ch;
+char c;
+InputStreamReader in = new InputStreamReader(System.in);
+BufferedReader br=new BufferedReader(in);
+S=br.readLine();
+S=S.toUpperCase();
+System.out.println("Enter your Name : "+S);
+N=S.length();
+System.out.println();
+System.out.println();
+for(int i=0;i<N;i++)
+{
+ch=S.charAt(i);
+switch(ch)
+{
+case 'A':
+System.out.println("..######..\n..#....#..\n..######..\n..#....#..\n..#....#..\n\n");
+
+System.out.println();System.out.println();
+break;
+case 'B':
+System.out.println("..######..\n..#....#..\n..#####...\n..#....#..\n..######..\n\n");
+System.out.println();
+System.out.println();
+  break;
+  case 'C':
+System.out.println("..######..\n..#.......\n..#.......\n..#.......\n..######..\n\n");
+System.out.println();
+System.out.println();
+break;
+case 'D':
+System.out.println("..#####...\n..#....#..\n..#....#..\n..#....#..\n..#####...\n\n");
+System.out.println();
+System.out.println();
+break;
+case 'E':
+System.out.println("..######..\n..#.......\n..#####...\n..#.......\n..######..\n\n");
+System.out.println();System.out.println();
+break;
+case 'F':
+System.out.println("..######..\n..#.......\n..#####...\n..#.......\n..#.......\n\n"); System .out.println();
+System.out.println ();
+break;
+
+case'G':
+System.out.println("..######..\n..#.......\n..#####...\n..#....#..\n..#####...\n\n"); System.out.println();
+System.out.println();
+break;
+
+case 'H':
+System.out.println("..#....#..\n..#....#..\n..######..\n..#....#..\n..#....#..\n\n");
+System.out.println();
+System.out.println();
+break;
+ 
+ case 'I':
+
+System.out.println("..######..\n....##....\n....##....\n....##....\n..######..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'J':
+
+System.out.println("..######..\n....##....\n....##....\n..#.##....\n..####....\n\n");
+System.out.println();
+System.out.println();
+break;
+
+case 'K':
+
+System.out.println("..#...#...\n..#..#....\n..##......\n..#..#....\n..#...#...\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'L':
+
+System.out.println("..#.......\n..#.......\n..#.......\n..#.......\n..######..\n\n");
+System.out.println();
+System.out.println();
+break;
+
+case 'M':
+
+System.out.println("..#....#..\n..##..##..\n..#.##.#..\n..#....#..\n..#....#..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'N':
+
+System.out.println("..#....#..\n..##...#..\n..#.#..#..\n..#..#.#..\n..#...##..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'O':
+
+System.out.println("..######..\n..#....#..\n..#....#..\n..#....#..\n..######..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'P':
+
+System.out.println("..######..\n..#....#..\n..######..\n..#.......\n..#.......\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'Q':
+
+System.out.println("..######..\n..#....#..\n..#.#..#..\n..#..#.#..\n..######..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'R':
+
+System.out.println("..######..\n..#....#..\n..#.##...\n..#...#...\n..#....#..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'S':
+
+System.out.println("..######..\n..#.......\n..######..\n.......#..\n..######..\n\n");
+System.out.println();
+System.out.println();
+
+break;
+
+case 'T':
+
+System.out.println("..######..\n....##....\n....##....\n....##....\n....##....\n\n");
+System.out.println();System.out.println();
+
+break;
+
+case 'U' :
+
+
+System.out.println("..#....#..\n..#....#..\n..#....#..\n..#....#..\n..######..\n\n");
+System.out.println();System.out.println();
+break;
+
+case 'V':
+
+System.out.println("..#....#..\n..#....#..\n..#....#..\n...#..#...\n....##....\n\n");
+System.out.println();System.out.println();
+
+break;
+
+case 'W':
+
+System.out.println("..#....#..\n..#....#..\n..#.##.#..\n..##..##..\n..#....#..\n\n");
+System.out.println();System.out.println();
+
+break;
+
+case 'X':
+System.out.println("..#....#..\n...#..#...\n....##....\n...#..#...\n..#....#..\n\n");
+System.out.println();System.out.println();
+break;
+
+case 'Y':
+System.out.println("..#....#..\n...#..#...\n....##....\n....##....\n....##....\n\n"); System.out.println();System.out.println();
+
+break;
+
+case 'Z':
+
+System.out.println("..######..\n......#...\n.....#....\n....#.....\n..######..\n\n");
+System.out.println();
+System.out.println();
+break;
+}}
+System.out.println("_______________________________________");
+System.out.println("Hope you will love the code . Follow me please and don't forget to leave an upvote😁😁");
+System.out.println("_______________________________________");
+
+}}
+
